158c6f7e67a24a684c21264306e80cd58ed599f0
mdemcsak <mdemcsak@masonlive.gmu.edu>
2014-03-25 21:35:31 -0400

    display books from db

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 	def index
 		@available_at = Time.now
-		@books = ["Atlas Shrugged", "Enders's Game", "Hamlet", "The Hobbit"]
+		@books = Book.all
 	end
 end
