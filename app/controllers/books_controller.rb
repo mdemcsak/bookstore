42dc73aa655b8d695efb6d5738eb23eaf2685ef7
mdemcsak <mdemcsak@masonlive.gmu.edu>
2014-04-01 20:01:31 -0400

    In class labs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,41 @@ class BooksController < ApplicationController
 		@books = Book.all
 	end
 
-  def show
-    @book = Book.find(params[:id])
-  end
+	def show
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		@book.save
+		redirect_to @book
+	end
+	
+	def edit
+	end
+
+	def update
+		@book.update(book_params)
+		redirect_to @book
+	end
+
+	def destroy
+		@book.destroy
+		redirect_to books_url
+	end	
+
+	private
+
+	def book_params
+	  	params.require(:book).permit(:title, :author, :pages, :price)
+	end
+
+	before_action :set_book, only: [ :show, :edit, :update, :destroy ]
+  
+	def set_book
+		@book = Book.find(params[:id])
+	end
 end
