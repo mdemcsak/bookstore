af104d411d4e0d80b433a63e4acfaf5f314ef127
Mike <mdemcsak@masonlive.gmu.edu>
2014-04-18 16:31:26 -0400

    Lab 11 updates

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ActiveRecord::Base
+    
+    has_many :reviews, dependent: :destroy
+    
 	scope :bargains, -> { where('price < 10.00') }
-	scope :by, ->(author) { where('author = ?', author) }	
+	
+    scope :by, ->(author) { where('author = ?', author) }	
 	
 	validates :title, :author, :pages, :price, presence: true
 	
@@ -11,4 +15,12 @@ class Book < ActiveRecord::Base
 	validates :price, 
 		numericality: { greater_than_or_equal_to: 0 },
 		if: "price.present?"
+    
+    def average_stars
+        if reviews.loaded?
+            reviews.map(&:stars).compact.average
+        else
+            reviews.average(:stars)
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Bookstore::Application.routes.draw do
+  resources :reviews
+
   resources :books do
-    get 'page/:page', :action => :index, :on => :collection
+      resources :reviews
+      get 'page/:page', :action => :index, :on => :collection
   end
   root 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
